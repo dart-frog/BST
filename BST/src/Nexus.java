8445032a8638f7a78a39c40bd1965fe1fd2395f0
ros_npswedberg <ros_npswedberg@WROSRM31919.seattleschools.org>
2013-12-17 09:55:53 -0800

    Added tree and nexus

---
+++ BST/src/Nexus.java
@@ -0,0 +1,12 @@
+
+public class Nexus {
+	Nexus leftChild;
+	Nexus rightChild;
+	int n;
+	public Nexus(int n){
+		this.n = n;
+	}
+	public int get(){
+		return n;
+	}
+}
---
+++ BST/src/Tree.java
@@ -0,0 +1,14 @@
+
+public class Tree {
+	public Tree(){
+		
+	}
+	public void add(int n){
+		Nexus x = new Nexus(n);
+		
+	}
+	public int get(int n){
+		return 0;
+		
+	}
+}
