cec815728801effe5c64dd4da62172e64f833ebd
ros_npswedberg <ros_npswedberg@WROSRM31919.seattleschools.org>
2013-12-20 10:56:47 -0800

    adding tunneling

--- BST/src/Nexus.java
+++ BST/src/Nexus.java
@@ -1,12 +1,24 @@
 
 public class Nexus {
-	Nexus leftChild;
-	Nexus rightChild;
+	Nexus leftChild = null;
+	Nexus rightChild = null;
 	int n;
 	public Nexus(int n){
 		this.n = n;
 	}
 	public int get(){
 		return n;
+	}
+	public Nexus getRightChild(){
+		return rightChild;
+	}
+	public Nexus getLeftChild(){
+		return rightChild;
+	}
+	public void setRightChild(int n){
+		rightChild.setRightChild(n);
+	}
+	public void setLeftChild(int n){
+		leftChild.setLeftChild(n);
 	}
 }
--- BST/src/Tree.java
+++ BST/src/Tree.java
@@ -1,14 +1,26 @@
 
 public class Tree {
-	public Tree(){
-		
+	private Nexus granddad; 
+	public Tree(Nexus granddad){
+		this.granddad = granddad;
 	}
 	public void add(int n){
-		Nexus x = new Nexus(n);
-		
+	
 	}
 	public int get(int n){
 		return 0;
 		
 	}
+	public void tunnel(Nexus nex  ,int n){
+		Nexus x = new Nexus(n);
+		if (nex.get() >= x.get()){
+			if (nex.getRightChild() == null){
+				nex.setRightChild(x.get());
+			}
+			else{
+				tunnel(nex.getRightChild(), n);
+			}
+		}
+	}
+	
 }
